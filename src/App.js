1233ecf55400d0de3d0681fb9a9e14fe29a07da9
Anthony Cordero <tony170593@gmail.com>
2023-08-21 20:31:47 -0600

    clase08-components-completa

--- src/App.js
+++ src/App.js
@@ -8,31 +8,54 @@ import Footer from './componentes/Footer';
 
 function App() {
   const [showHideForm, setShowHideForm] = useState(true);
+
   const [coworkers, updateCoworwers] = useState([
     {
       name: "Anthony Cordero",
       position: "Programador",
       photo: "https://github.com/AnthonyCordero.png",
       team: "Front End"
-    }
+    },
+    {
+      name: "Genesys Rondón",
+      position: "Desarrolladora de software e instructora",
+      photo: "https://scontent.fsyq1-1.fna.fbcdn.net/v/t39.30808-6/369161947_6347246988716822_473931346132967735_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=6qcwtDBjk2IAX_e7gsW&_nc_ht=scontent.fsyq1-1.fna&oh=00_AfDgdql2OL-tvcU0WHN9Dxp7cvpWxdz0IMzTcgs5IBFNnw&oe=64E8CA1F",
+      team: "Front End"
+    },
+    {
+      name: "Jeanmarie Quijada",
+      position: "Instructora en Alura Latam",
+      photo: "https://github.com/JeanmarieAluraLatam.png",
+      team: "Front End"
+    },
+    {
+      name: "Jose Gonzalez",
+      position: "Dev. FullStack",
+      photo: "https://github.com/JoseDarioGonzalezCha.png",
+      team: "Data Science"
+    },
+    {
+      name: "Harland Lohora",
+      position: "Intructor",
+      photo: "https://github.com/harlandlohora.png",
+      team: "Front End"
+    },
+    {
+      name: "Jeanmarie Quijada",
+      position: "Instructora en Alura Latam",
+      photo: "https://github.com/AnthonyCordero.png",
+      team: "Innovación y Gestión"
+    },
+    {
+      name: "Christian Velasco",
+      position: "Head de Alura e instructor",
+      photo: "https://github.com/christianpva.png",
+      team: "Móvil"
+    },
+
   ]);
 
-  // Ternario --> condicion ? seMuestra : noSeMuestra
-  const switchShowHide = () => {
-    setShowHideForm(!showHideForm)
-  }
-
-  //CoworkerRegistration
-
-  const coworkerRegistration = (coworker) => {
-    console.log('Nuevo Colaborador', coworker);
-    //SpreadOperator
-    updateCoworwers([...coworkers, coworker])
-  }
-
-  //Lista de equipos
-
-  const teamsInfo = [
+  const [teamsInfo, updateTeamsInfo] = useState([
     {
       title: 'Programación',
       primaryColor: '#57C278 ',
@@ -68,7 +91,41 @@ function App() {
       primaryColor: ' #FF8A29',
       secondaryColor: '#FFEEDF'
     },
-  ]
+  ])
+
+  // Ternario --> condicion ? seMuestra : noSeMuestra
+  const switchShowHide = () => {
+    setShowHideForm(!showHideForm)
+  }
+
+  //CoworkerRegistration
+
+  const coworkerRegistration = (coworker) => {
+    console.log('Nuevo Colaborador', coworker);
+    //SpreadOperator
+    updateCoworwers([...coworkers, coworker])
+  }
+
+  // Eliminar Colaborador
+
+  const deleteCoworker = () => {
+    console.log('Eliminar');
+  }
+
+  // Actualizad Color de Equipo
+
+  const updateColor = (color, title) => {
+    console.log('Acualizar: ', color, title);
+    const updatedTeamsColor = teamsInfo.map((team) => {
+      if (team.title === title) {
+        team.primaryColor = color;
+      }
+      return team;
+    });
+
+    updateTeamsInfo(updatedTeamsColor);
+  };
+
 
   return (
     <div>
@@ -89,6 +146,8 @@ function App() {
           teamData={defTeam}
           key={defTeam.title}
           coworkers={coworkers.filter(coworker => coworker.team === defTeam.title)}
+          deleteCoworker={deleteCoworker}
+          updateColor={updateColor}
         />
         )
       }
--- src/componentes/Team/index.js
+++ src/componentes/Team/index.js
@@ -1,24 +1,32 @@
 import './styles.css'
 import Coworker from '../Coworker'
+import hexToRgba from 'hex-to-rgba';
 
 
 const Team = (props) => {
     //Destructuracion
     const { title, primaryColor, secondaryColor } = props.teamData
-    const { coworkers } = props
-    const teamSectionBG = { backgroundColor: secondaryColor }
+    const { coworkers, deleteCoworker, updateColor } = props
+    const teamSectionBG = { backgroundColor: hexToRgba(primaryColor, 0.3) }
     const underLineColor = { borderColor: primaryColor }
 
     return <>
         {
             coworkers.length > 0 &&
             < section className="teamSection" style={teamSectionBG} >
+                <input
+                    type='color'
+                    className='input-color'
+                    value={primaryColor}
+                    onChange={(e) => { updateColor(e.target.value, title) }}
+                />
                 <h3 style={underLineColor}>{title}</h3>
                 <div className="colleagues">
                     {coworkers.map((coworker, index) => <Coworker
                         data={coworker}
                         key={index}
                         primaryColor={primaryColor}
+                        deleteCoworker={deleteCoworker}
                     />)
                     }
                 </div>
