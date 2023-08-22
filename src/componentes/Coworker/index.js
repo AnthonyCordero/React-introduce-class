70056d6b2c0eece2edf17b3c29d10b7cab77947e
Anthony Cordero <tony170593@gmail.com>
2023-08-21 21:49:42 -0600

    clase09-functionalities-complete

--- src/componentes/Coworker/index.js
+++ src/componentes/Coworker/index.js
@@ -2,11 +2,11 @@ import './styles.css'
 import { AiFillCloseCircle } from 'react-icons/ai'
 
 const Coworker = (props) => {
-    const { name, position, photo, team } = props.data
+    const { name, position, photo, team, id } = props.data
     const { primaryColor, deleteCoworker } = props
 
     return <div className='coworker'>
-        <AiFillCloseCircle className='delete' onClick={deleteCoworker} />
+        <AiFillCloseCircle className='delete' onClick={() => deleteCoworker(id)} />
         <div className='coworkertittle' style={{ backgroundColor: primaryColor }}>
             <img src={photo} alt={name} />
         </div>
